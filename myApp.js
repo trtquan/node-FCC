c1ae3d47fdb53fac15aeeae3e93e83229f0558eb
quantran <trtquan@gmail.com>
2020-09-18 12:58:03 +0700

    update asset

--- myApp.js
+++ myApp.js
@@ -17,6 +17,7 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/views/index.html")
 });
 /** 4) Serve static assets  */
+app.use(express.static(__dirname + "/public"));
 app.use('/public', express.static(__dirname + "/public"));
 /** 5) serve JSON on a specific route */
 
