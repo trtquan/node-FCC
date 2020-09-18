8e47fa77ef74300ae7d8a2a7f4356ccf58980fb6
quantran <trtquan@gmail.com>
2020-09-18 14:04:21 +0700

    updateLog

--- myApp.js
+++ myApp.js
@@ -2,7 +2,10 @@ var express = require("express");
 var app = express();
 require('dotenv').config();
 // --> 7)  Mount the Logger middleware here
+app.use(function(req, res, next) {
+	console.log((req.method+ ' ' + req.path+ ' ' + req.ip));
 
+})
 // --> 11)  Mount the body-parser middleware  here
 
 /** 1) Meet the node console. */
