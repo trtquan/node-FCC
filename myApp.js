071e896bc7f39c1a10c1ec07e11dd4a1973f20d0
quantran <trtquan@gmail.com>
2020-09-18 13:21:32 +0700

    res json

--- myApp.js
+++ myApp.js
@@ -19,7 +19,9 @@ app.get('/', (req, res) => {
 /** 4) Serve static assets  */
 app.use(express.static(__dirname + "/public"));
 /** 5) serve JSON on a specific route */
-
+app.get('json', ((req, res) => {
+	res.json({message: "Hello json"})
+}));
 /** 6) Use the .env file to configure the app */
 
 /** 7) Root-level Middleware - A logger */
