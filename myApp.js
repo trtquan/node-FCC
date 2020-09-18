2ce90617d277542af276af1aa9beade489a1451f
quantran <trtquan@gmail.com>
2020-09-18 15:19:34 +0700

    update param

--- myApp.js
+++ myApp.js
@@ -51,7 +51,8 @@ app.get("/:word/echo", (req, res) => {
 })
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-app.get('name', (req, res) => {
+app.get('/name', (req, res) => {
+	console.log(`${req.query.first} ${req.query.last}`, "<-this");
 	res.json({name: `${req.query.first} ${req.query.last}`});
 });
 /** 11) Get ready for POST Requests - the `body-parser` */
