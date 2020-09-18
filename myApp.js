ebbbb6ccf7abd402fe4e51d7ab976f47d3a69d22
quantran <trtquan@gmail.com>
2020-09-18 12:45:47 +0700

    update myApp return html file

--- myApp.js
+++ myApp.js
@@ -9,11 +9,13 @@ var app = express();
 console.log("Hello World");
 
 /** 2) A first working Express Server */
-app.get('/', (req, res) => {
-	res.send("Hello Express");
-})
+// app.get('/', (req, res) => {
+// 	res.send("Hello Express");
+// })
 /** 3) Serve an HTML file */
-
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + "/views/index.html")
+});
 /** 4) Serve static assets  */
 
 /** 5) serve JSON on a specific route */
