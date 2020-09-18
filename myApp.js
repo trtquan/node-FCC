ad7c925582c85cab14d09252fa7f914e737cbff6
quantran <trtquan@gmail.com>
2020-09-18 13:15:58 +0700

    update static

--- myApp.js
+++ myApp.js
@@ -18,7 +18,6 @@ app.get('/', (req, res) => {
 });
 /** 4) Serve static assets  */
 app.use(express.static(__dirname + "/public"));
-app.use('/public', express.static(__dirname + "/public"));
 /** 5) serve JSON on a specific route */
 
 /** 6) Use the .env file to configure the app */
